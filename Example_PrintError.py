a59f4b6a0f837b76a2f4e372eb4a9bb35f3f5c0c
Derek Frost <devifr@gmail.com>
2022-03-31 16:55:51 -0400

    make more general try/except

--- Example_PrintError.py
+++ Example_PrintError.py
@@ -1,4 +1,28 @@
-try:
-	print(5/0)
-except ZeroDivisionError:
-	print('you cannot divide by z')
+
+#########################################
+### Nested Try/Except in a While Loop ###
+#########################################
+
+import time
+
+while True:
+    
+    
+    try:
+        x = int(input('Enter a value to divide by 5:'))
+        print(5/x)
+        
+    except:
+        print('this is a general error @ ', time.asctime())
+        pass
+    # except ZeroDivisionError:
+    #     print('this is a zero error @ ', time.asctime())
+    #     pass  
+    # except TypeError:
+    #     print('This is a type error @ ', time.asctime())
+    #     pass     
+    # except ValueError:
+    #     print('This is a value error @ ', time.asctime())
+    #     pass
+    
+    time.sleep(3)
