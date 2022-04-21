8ab18ac9e22529f9f40b53cb763340ffcd14bc0b
Derek Frost <devifr@gmail.com>
2022-04-21 10:39:40 -0400

    Solution for IndexError

--- Example_Multi_Index.py
+++ Example_Multi_Index.py
@@ -20,3 +20,12 @@ df = pd.DataFrame(np.random.randn(7, 3), index=index, columns=['A', 'B', 'C'])
 
 df.groupby(level='first').tail(1)
 
+#%%
+
+# There are three columns in the DF, .iloc[:,3] is asking for the 4th column
+# Return IndexError: single positional indexer is out-of-bounds
+try:
+    df.iloc[:,4]
+except:
+    print('there is some error')
+
