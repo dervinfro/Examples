edeadf732ad1e97b049f2511fb2e95623ba978f5
dervinfro <44220322+dervinfro@users.noreply.github.com>
2022-05-02 21:38:34 -0400

    Update Example_Send_SMS_Twilio.py
    remove auth token

--- Example_Send_SMS_Twilio.py
+++ Example_Send_SMS_Twilio.py
@@ -19,7 +19,7 @@ from twilio.rest import Client
 
 
 account_sid = 'AC461e0746b12ba3a1aa62b5642c4886c7'
-auth_token = 'b3b10e414b150afc486ca7c797311f29'
+auth_token = 'enter_auth_id'
 
 client = Client(account_sid, auth_token)
 
