f5c768f5238fcdea5575bff419337b3ee2621aa2
Derek Frost <devifr@gmail.com>
2023-05-07 23:12:36 -0400

    initial commit

---
+++ Example_Empty_List.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 16:42:49 2022
+
+@author: derekfrost
+"""
+# How to test the condition of a empty list
+a = []
+b = [0]
+
+print(not a)  # Returns: True
+print(not b)  # Returns: False
