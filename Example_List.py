da5ecc3c5b1b03f3099532dbe7affa8f1a6a277a
Derek Frost <devifr@gmail.com>
2023-08-05 23:54:36 -0400

    comments, leet 118 and arrays

--- Example_List.py
+++ Example_List.py
@@ -33,3 +33,28 @@ if guns[3] == 'cz':
 else:
 	print('no AK')
 	
+
+#%%
+import itertools
+"""
+This section is a scratch pad for Leet 118 - Pascal Triangle
+I am looking at ways of creating summed pair from an array and then inserting
+those summed pair results into a list.
+Using itertools.pairwise to sum the array and a simple assignment operator
+ie array[1:1]...the reason this is [1:1] is that all the sublist matrices 
+START and END with 1.  Inserting the summed pairs from the previous sublist
+matrix is the solution to this problem.
+"""
+
+# Establish a multi matrix array/list
+array = [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+# Pull one of the elements out of the array and assign it to an object
+array1 = array[1]
+
+
+# Pull elements from the last matix of the array and insert them into array1
+array1[1:1] = array[4][1:4]
+
+# Create a FOR loop that will pairwise the array and output the sums of the pairs
+sum_array= [(x+y) for (x, y) in itertools.pairwise(array1)]
