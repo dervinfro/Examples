ce7c57911a8d0ec2f0b9e947fdf4fc1539f08d40
Derek Frost <devifr@gmail.com>
2023-08-22 23:31:53 -0400

    minor code changes...NSTR

--- Example_DateTime_Conversions.py
+++ Example_DateTime_Conversions.py
@@ -7,11 +7,11 @@ Created on Thu Sep 30 22:08:12 2021
 """
 import pandas as pd
 import numpy as np
-import openpyxl
+# import openpyxl
 
 # Create a random integer to use as the microsecond for the time.
 # This will make each date/time unique
-random_int = np.random.randint(10000,50000)
+random_int = np.random.randint(10000, 50000)
 
 # Test dataset that will return a df of DateTimeIndex type
 df = pd.date_range('1/1/2021 01:00:00.000', periods=5, freq='H')
